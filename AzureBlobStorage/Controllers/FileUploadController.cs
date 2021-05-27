7717576c607fb61c5b855a1bf659344dc1ee0b51
tarunrpmahar <tsmahar1997@gmail.com>
2021-05-27 17:07:33 +0530

    Add project files.

---
+++ AzureBlobStorage/Controllers/FileUploadController.cs
@@ -0,0 +1,34 @@
+﻿using AzureBlobStorage.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AzureBlobStorage.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileUploadController : ControllerBase
+    {
+        private readonly IStorageService _storageService;
+
+        public FileUploadController(IStorageService storageService)
+        {
+            _storageService = storageService;
+        }
+        public IActionResult Get()
+        {
+            return Ok("File Upload api running...");
+        }
+
+        [HttpPost]
+        [Route("upload")]
+        public IActionResult Upload(IFormFile file)
+        {
+            _storageService.Upload(file);
+            return Ok();
+        }
+    }
+}
---
+++ AzureBlobStorage/Startup.cs
@@ -0,0 +1,55 @@
+using AzureBlobStorage.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Azure;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AzureBlobStorage
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddTransient<IStorageService, StorageService>();
+            services.AddAzureClients(builder =>
+            {
+                builder.AddBlobServiceClient(Configuration.GetSection("Storage:ConnectionString").Value);
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
